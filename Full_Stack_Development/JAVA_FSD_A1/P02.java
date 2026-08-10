ca365f7083da9dad7df28ff117693e53459f144d
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-08-11 04:50:30 +0530

    Add files via upload

---
+++ Full_Stack_Development/JAVA_FSD_A1/P02.java
@@ -0,0 +1,19 @@
+//P02: Arithmetic Operations
+import java.util.Scanner;
+public class P02 
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the valuen of a : ");
+        int a=sc.nextInt();
+        System.out.println("Enter the valuen of b : ");
+        int b=sc.nextInt();
+        System.out.println("Sum : " + (a + b));
+        System.out.println("Difference : " + (a - b));
+        System.out.println("Product : " + (a * b));
+        System.out.println("Quotient : " + (a / b));
+        System.out.println("Remainder : " + (a % b));
+        System.out.println("Exact : "+ (a/(double)b));
+    }
+}
