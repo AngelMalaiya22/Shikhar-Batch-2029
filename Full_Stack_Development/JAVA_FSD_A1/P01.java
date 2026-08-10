7df6f5d219aeb227534bd8353c64e2ab047a6d1f
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-08-11 04:50:14 +0530

    Add files via upload

---
+++ Full_Stack_Development/JAVA_FSD_A1/P01.java
@@ -0,0 +1,12 @@
+// P01: Hello, Java! - Topic: printing & escape sequences
+
+public class P01
+{
+    public static void main(String args[])
+    {
+        System.out.println("Name : Angel Malaiya");
+        System.out.println("System ID : 2025222124");
+        System.out.println("Branch : CSE AIML(2nd Year)");
+        System.out.println("First, solve the problem. Then, write the code.");
+    }
+}
