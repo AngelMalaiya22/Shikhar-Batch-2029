6914e4880316db5d38e50fea976bcb09d5bd84d0
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-08-14 18:28:37 +0530

    Add files via upload

---
+++ DSA_In_JAVA/Basics_Revision_Sheet_of_JAVA/EvenOrOdd.java
@@ -0,0 +1,18 @@
+// WAP to check whether a number is even or odd.
+import java.util.Scanner;
+public class EvenOrOdd 
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int num=sc.nextInt();
+        if(num%2==0)
+        {
+            System.out.println("The number is even");
+        }
+        else
+        {
+            System.out.println("The number is odd");
+        }
+    }
+}
