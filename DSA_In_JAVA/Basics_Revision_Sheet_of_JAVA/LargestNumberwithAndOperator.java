295f17876a5ae6e7edeb6f8a9c767e6e27741252
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-08-14 18:29:22 +0530

    Add files via upload

---
+++ DSA_In_JAVA/Basics_Revision_Sheet_of_JAVA/LargestNumberwithAndOperator.java
@@ -0,0 +1,24 @@
+// / WAP to take input 3 numbers from the user and print the largest of them using AND operator.
+import java.util.Scanner;
+public class LargestNumberwithAndOperator 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int num3=sc.nextInt();
+        if(num1>num2 && num1>num3)
+        {
+            System.out.println("The largest number is: "+num1);
+        }
+        else if(num2>num1 && num2>num3)
+        {
+            System.out.println("The largest number is: "+num2);
+        }
+        else
+        {
+            System.out.println("The largest number is: "+num3);
+        }
+    }
+}
