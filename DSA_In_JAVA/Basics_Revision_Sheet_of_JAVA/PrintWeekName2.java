c83f6714fbe4accbbf48d3bede6b99e780f7b0c7
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-08-17 05:04:07 +0530

    Add files via upload

---
+++ DSA_In_JAVA/Basics_Revision_Sheet_of_JAVA/PrintWeekName2.java
@@ -0,0 +1,52 @@
+// Wap to take input (range 1 to 7) from the user and print the corresponding week using switch case.
+import java.util.Scanner;
+public class PrintWeekName2 
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int choice=sc.nextInt();
+        switch(choice)
+        {
+            case 1:
+            {
+                System.out.println("Monday");
+                break;
+            }
+            case 2:
+            {
+                System.out.println("Tuesday");
+                break;
+            }
+            case 3:
+            {
+                System.out.println("Wednesday");
+                break;
+            }
+            case 4:
+            {
+                System.out.println("Thrusday");
+                break;
+            }
+            case 5:
+            {
+                System.out.println("Friday");
+                break;
+            }
+            case 6:
+            {
+                System.out.println("Saturday");
+                break;
+            }
+            case 7:
+            {
+                System.out.println("Sunday");
+                break;
+            }
+            default:
+            {
+                System.out.println("Invaild Choice");
+            }
+        }
+    }
+}
